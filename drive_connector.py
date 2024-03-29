8f8349c5fe55a9449af9ea436687e4d55f3694fa
ProfessionAI <37979754+ProfAI@users.noreply.github.com>
2024-03-29 17:48:46 +0100

    Create drive_connector.py

---
+++ drive_connector.py
@@ -0,0 +1,72 @@
+from google.colab import auth
+from google.auth import default
+from googleapiclient.discovery import build
+import gspread
+import pandas as pd
+
+
+class DriveConnector:
+
+  def __init__(self, drive_id):
+    self._drive_id = drive_id
+    self._service = build('drive', 'v3')
+    self._creds, _ = default()
+    auth.authenticate_user()
+
+
+  def _get_folder_id(self, folder_name):
+
+    response = service.files().list(q=f"name='{folder_name}' and mimeType='application/vnd.google-apps.folder'",
+                                    spaces='drive',
+                                    driveId=self._drive_id,
+                                    corpora="drive",
+                                    includeItemsFromAllDrives=True,
+                                    supportsAllDrives=True,
+                                    fields='files(id, name)').execute()
+
+    for folder in response.get('files', []):
+        return folder.get('id')
+
+    return None
+
+
+  def _get_file_id(self, file_name, folder_id):
+    
+    response = service.files().list(q=f"name='{file_name}' and '{folder_id}' in parents",
+                                  spaces='drive',
+                                  driveId=self._drive_id,
+                                  corpora="drive",
+                                  includeItemsFromAllDrives=True,
+                                  supportsAllDrives=True,
+                                  fields='files(id, name)').execute()
+
+    for file in response.get('files', []):
+        return file.get('id')
+
+    return None
+
+
+  def get_gsheet_as_df(self, file_name, folder_name):
+
+    folder_id = self._get_folder_id(folder_name)
+    file_id = self._get_file_id(file_name, folder_id)
+
+    gc = gspread.authorize(creds)
+    spreadsheet_url = 'https://docs.google.com/spreadsheets/d/'+file_id
+    sheet = gc.open_by_url(spreadsheet_url).sheet1
+    df = self._gsheet_to_df(sheet)
+    return df
+
+
+  def _gsheet_to_df(self, sheet):
+    data = sheet.get_all_values()
+    df = pd.DataFrame(data)
+    df.columns = df.iloc[0]
+    df = df.iloc[1:]
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+
+
+df = DriveConnector(SHARED_DRIVE_ID).get_gsheet_as_df(FILE_NAME, COURSE_ID)
+df.head()
